b0dca50b3466ca4481f2fcc294c5e900bcd06c0e
Eduardo Zenere <eduardo@ezenere.com>
2024-11-21 05:47:14 -0300

    changes to mobile layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default async function Home() {
   const items = await Collections();
 
   return (
-    <div className="max-w-[1200px] m-auto flex mt-12 gap-5">
-      {items.map((i) => <Link key={i.id} href={`/${i.id}`} className="w-[30%] border border-black rounded-md p-3 cursor-pointer transition-colors bg-slate-200 hover:bg-slate-300">
+    <div className="max-w-[1200px] m-auto mt-12 grid-container gap-5 pr-3 pl-3">
+      {items.map((i) => <Link key={i.id} href={`/${i.id}`} className="border border-black rounded-md p-3 cursor-pointer transition-colors bg-slate-200 hover:bg-slate-300">
         <div className="flex flex-row">
           <div className="flex-1 text-base">{i.name}</div>
           <div className="flex-initial text-xs">{i.quantity} cards</div>
